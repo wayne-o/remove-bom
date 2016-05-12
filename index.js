8139a7c9cc8442f796c27445045318c6137a131c
wayne-o <wayne@prjatk.com>
2016-05-12 11:08:57 +0100

    removing boms

---
+++ index.js
@@ -0,0 +1,27 @@
+var brocWriter = require('broccoli-writer');
+var replace = require('replace');
+module.exports = RemoveBom;
+RemoveBom.prototype = Object.create(brocWriter.prototype);
+RemoveBom.prototype.constructor = RemoveBom;
+RemoveBom.prototype.description = 'remove BOM from js files';
+
+function RemoveBom(inTree, options) {
+	if (!(this instanceof RemoveBom)) {
+		return new RemoveBom(inTree, options);
+	}
+	this.inTree = inTree;
+	this.options = options || {};
+}
+
+RemoveBom.prototype.write = function(readTree, destDir) {
+	var self = this;
+	return readTree(this.inTree)
+		.then(function(srcDir) {
+			replace({
+				regex: "\uFEFF",
+				replacement: "",
+				paths: [srcDir + self.options],
+				silent: false
+			});
+		});
+};
